822d7ed3d472796a6e29a1b54a69e410496b37a8
nishizatokaisyuu0305 <82567255+nishizatokaisyuu0305@users.noreply.github.com>
2022-10-14 10:31:19 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+
+ $title = "PHPテスト";
+ $content = 'PHPテストです';
+ $post_at = '2022/10/14';
+ $tag = ['PHP','プログラミング'];
+ $status = true; 
+
+
+
+ const ID = 1;
+
+ echo ID;
+ echo '<br>';
+ echo $title;
+ echo '<br>';
+ echo $content;
+ echo '<br>';
+ echo $post_at;
+ echo '<br>';
+ print_r($tag);
+
+
+ var_dump(ID);
+
+
+ echo '<br>';
+ echo "タイトル名:$title";
+ echo '<br>';
+ echo 'タイトル名：$title';
+
+?>
